1b63eff71d01ca26355d0baa056e282ac4201341
Francisco Javier Blasco Pascual <84475547+jblasco2@users.noreply.github.com>
2022-01-20 09:47:36 +0100

    Update classify.R

--- codigo/classify.R
+++ codigo/classify.R
@@ -1,3 +1,17 @@
+## Creamos el corpus de la siguiente manera: dos carpetas ('primary_set' y 'secondary_set')
+## En la primera (primary_set), de entrenamiento, colocamos todos los textos de los candidatos.
+## En la segunda (secondary_set), de testado, colocamos los textos objeto de nuestro análisis
+
+# Establecemos directorio de trabajo
 setwd("~/Desktop/TEDEX_2/POLITICA/corpus_classify")
+
+#Cargamos 'stylo'
 library(stylo)
-classify()
+
+# Ejecutamos la función
+classify(gui = TRUE, training.frequencies = NULL, 
+         test.frequencies = NULL,
+         training.corpus.dir = "primary_set", 
+         test.corpus.dir = "secondary_set", 
+         features = NULL, 
+         path = NULL)
