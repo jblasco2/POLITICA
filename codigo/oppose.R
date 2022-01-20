c09eea09304f771d1e43990207794641bdeef96d
Francisco Javier Blasco Pascual <84475547+jblasco2@users.noreply.github.com>
2022-01-20 10:07:50 +0100

    Update oppose.R

--- codigo/oppose.R
+++ codigo/oppose.R
@@ -1,4 +1,20 @@
-setwd("~/Desktop/TEDEX_2/POLITICA/corpus_oppose")
+## Creamos el corpus de la siguiente manera: dos carpetas ('primary_set' y 'secondary_set')
+## En la primera (primary_set) colocamos todos los textos de un autor.
+## En la segunda (secondary_set) colocamos los textos del segundo autor
+## Procedemos a comparar los léxicos
+
+# Establecemos directorio de trabajo
+setwd("~/Desktop/POLITICA/corpus_oppose")
+
+#Cargamos 'stylo'
 library(stylo)
-oppose()
+
+# Ejecutamos la función
+oppose(gui = TRUE, training.frequencies = NULL, 
+         test.frequencies = NULL,
+         training.corpus.dir = "primary_set", 
+         test.corpus.dir = "secondary_set", 
+         features = NULL, 
+         path = NULL)
+
 
